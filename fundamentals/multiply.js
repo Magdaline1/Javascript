0016b9b654ca52e93c35a68899e7f230854c3382
Magdaline <mamoakohene@gmail.com>
2022-12-13 13:14:20 +0000

    Section 3 & 4, exercises and challenges

---
+++ fundamentals/multiply.js
@@ -0,0 +1,6 @@
+const multiply = (a, b) => {
+  return a * b;
+}
+
+// console.log(multiply(10, 5))
+module.exports = multiply
---
+++ fundamentals/sayHello.js
@@ -0,0 +1,6 @@
+const sayHello = (name) => {
+  return `Hello ${name}`;
+  // this is equal to return 'Hello ' + name
+}
+
+console.log(sayHello('Magdaline'))
