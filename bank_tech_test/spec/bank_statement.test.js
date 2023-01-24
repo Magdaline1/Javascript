54a163ba7f8e55f85e8cd7d45e14c6261f76740e
Magdaline <mamoakohene@gmail.com>
2023-01-24 23:16:42 +0000

    fixing BankStatement test issue - all tests now pass

--- bank_tech_test/spec/bank_statement.test.js
+++ bank_tech_test/spec/bank_statement.test.js
@@ -1,15 +1,16 @@
 const BankStatement = require("../bank_statement")
+const Transaction = require("../transaction")
 
 describe("BankStatement", () => {
   it("shows 0 balance if no transactions have been made", () => {
-    const bankStatement = new BankStatement
+    const bankStatement = new BankStatement()
 
     expect(bankStatement.currentBalance()).toEqual(0)
   });
 
 
   it("adds transactions to the bank balance", () => {
-    const bankStatement = new BankStatement
+    const bankStatement = new BankStatement()
 
     bankStatement.addTransaction(new Transaction("deposit", 500))
     bankStatement.addTransaction(new Transaction("deposit", 500))
@@ -19,11 +20,14 @@ describe("BankStatement", () => {
 
 
   it("prints a statement for the user", () => {
-    const bankStatement = new BankStatement
+    const bankStatement = new BankStatement()
+
+    const logSpy = jest.spyOn(global.console, 'log');
 
     bankStatement.addTransaction(new Transaction("deposit", 500))
-    bankStatement.currentBalance()
-    expect(bankStatement.statement()).toContain("Tue Jan 24 2023  ||  500  ||  0  ||  500")
-  });
+    bankStatement.statement()
 
+    expect(logSpy).toHaveBeenCalledWith("Tue Jan 24 2023  ||  500  ||  0  ||  500");
+  });
+  
 });
